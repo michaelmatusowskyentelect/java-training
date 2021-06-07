2822dad1824270e18cb7cf9553c7f39d665f0dcf
Michael Matusowsky <michael.matusowsky@entelect.co.za>
2021-06-07 22:11:25 +0200

    refactored code into methods

--- Section 4/CodeBlocksAndIfThenElseControlStatements/src/com/company/Main.java
+++ Section 4/CodeBlocksAndIfThenElseControlStatements/src/com/company/Main.java
@@ -3,21 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 5000;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-        if (score == 5000)
-            System.out.println("Your score was 5000");
+        int highscore = calculateScore(true, 5, 10, 3);
+        System.out.println("Your final score was " + highscore);
+        highscore = calculateScore(false, 3, 3, 3);
+        System.out.println("Your final score was " + highscore);
 
-        gameOver = true;
-        int newScore = 10000;
-        int newLevelCompleted = 8;
-        int newBonus = 200;
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         if (gameOver) {
-            int finalScore = newScore + (newLevelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            return finalScore;
         }
+
+        return -1;
     }
 }
