d6b05d6258ecac0a0583e15c5af3acbaf202aa55
Michael Matusowsky <michael.matusowsky@entelect.co.za>
2021-06-08 09:02:55 +0200

    methods challenge

--- Section 4/CodeBlocksAndIfThenElseControlStatements/src/com/company/Main.java
+++ Section 4/CodeBlocksAndIfThenElseControlStatements/src/com/company/Main.java
@@ -9,6 +9,11 @@ public class Main {
         highscore = calculateScore(false, 3, 3, 3);
         System.out.println("Your final score was " + highscore);
 
+        displayHighScorePosition("Michael", calculateHighScorePosition(1500));
+        displayHighScorePosition("Joseph", calculateHighScorePosition(900));
+        displayHighScorePosition("Renato", calculateHighScorePosition(400));
+        displayHighScorePosition("Sasha", calculateHighScorePosition(50));
+        displayHighScorePosition("Sasha", calculateHighScorePosition(1000));
 
     }
 
@@ -21,5 +26,20 @@ public class Main {
         }
 
         return -1;
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score >= 100) {
+            return 3;
+        }
+        return 4;
     }
 }
