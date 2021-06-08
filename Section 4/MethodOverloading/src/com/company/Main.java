abedba5a4c22c52c265e17697b80caff839892fb
Michael Matusowsky <michael.matusowsky@entelect.co.za>
2021-06-08 12:12:02 +0200

    implemented challenge

--- Section 4/MethodOverloading/src/com/company/Main.java
+++ Section 4/MethodOverloading/src/com/company/Main.java
@@ -3,6 +3,42 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int newScore = calculateScore("Michael", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+        System.out.println(calcFeetAndInchesToCentimeters(6d));
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player" + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
+
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if (!(feet >= 0) || !(inches >=0 || inches < 12)) {
+            return -1;
+        }
+        return (feet*12 + inches)*2.54 ;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (!(inches >= 0.0)){
+            return -1;
+        }
+        double feet = (inches-inches%12)/12.0;
+        inches = inches%12;
+        return calcFeetAndInchesToCentimeters(feet, inches);
     }
 }
