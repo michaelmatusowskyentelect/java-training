68cb067660aeccfcad1611571ebd80f5090904c4
Michael Matusowsky <michael.matusowsky@entelect.co.za>
2021-06-10 16:44:12 +0200

    end of inheritance

--- Section 6/Inheritance/src/com/company/Dog.java
+++ Section 6/Inheritance/src/com/company/Dog.java
@@ -8,12 +8,44 @@ public class Dog extends Animal {
     private int teeth;
     private String coat;
 
-    public Dog(String name, int brain, int body, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
-        super(name, brain, body, size, weight);
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
         this.eyes = eyes;
         this.legs = legs;
         this.tail = tail;
         this.teeth = teeth;
         this.coat = coat;
+    }
+
+    private void chew(){
+        System.out.println("Dog.chew() called.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called.");
+        chew();
+        super.eat();
+    }
+
+    public void walk() {
+        System.out.println("Dog.walk() called.");
+        move(5);
+    }
+
+    public void run() {
+        System.out.println("Dog.run() called.");
+        move(25);
+    }
+
+    private void moveLegs(int speed){
+        System.out.println("Dog.moveLegs() called.");
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called.");
+        moveLegs(speed);
+        super.move(speed);
     }
 }
