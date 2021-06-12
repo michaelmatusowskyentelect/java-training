758c12f8dce332f924a732a6e8cbd976ddea780f
Michael Matusowsky <michael.matusowsky@entelect.co.za>
2021-06-12 10:15:55 +0200

    Finshed challenge

--- Section 7/EncapsulationChallenge/src/com/company/Printer.java
+++ Section 7/EncapsulationChallenge/src/com/company/Printer.java
@@ -2,5 +2,15 @@ package com.company;
 
 public class Printer {
 
+    private Toner toner;
+    private int printedPages;
+    private boolean duplexPrinter;
+
+    public Printer(Toner toner, int printedPages, boolean duplexPrinter) {
+        this.toner = toner;
+        this.printedPages = printedPages;
+        this.duplexPrinter = duplexPrinter;
+    }
+
 
 }
---
+++ Section 7/EncapsulationChallenge/src/com/company/Toner.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Toner {
+
+    private int tonerLevel;
+
+    public Toner(int tonerLevel) {
+        this.tonerLevel = tonerLevel;
+    }
+
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
+
+    public void setTonerLevel(int tonerLevel) {
+        this.tonerLevel = tonerLevel;
+    }
+}
